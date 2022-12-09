ec876bf3ced70e13af3b8c7af1b0ca73544ea4b6
tanaliga100 <jordantanaliga@gmail.com>
2022-12-09 14:01:17 +0800

    rollback-toV18

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    host: '0.0.0.0'
+  server: {
+    host: "0.0.0.0",
   },
-  build: {
-    
-  }
-})
+});
